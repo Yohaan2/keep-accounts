fe61d05bfda38b91afa2697995962a224a85733d
yohaan2 <garciayohan57@gmail.com>
2024-07-17 14:39:16 -0400

    config test

--- test/user.spec.ts
+++ test/user.spec.ts
@@ -2,7 +2,7 @@ import { assert } from 'chai'
 
 describe('User', () => {
 	it('should be able to create a user', () => {
-		console.log('tests')
+		console.log('tests2')
 		assert.isTrue(true)
 	})
 })
